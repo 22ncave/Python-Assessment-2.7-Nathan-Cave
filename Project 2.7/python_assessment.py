b3cfa5bfa3490dabe189e7f360f090b31595adc3
22ncave <22ncave@wakatipu.school.nz>
2025-03-27 13:22:26 +1300

    Update python_assessment.py

--- Project 2.7/python_assessment.py
+++ Project 2.7/python_assessment.py
@@ -1,24 +1,32 @@
 order = []
 pizza_list = [["Ham & Cheese",13.99], ["Pepperoni",12.49], ["Vegetarian",14.99], ["Cheese",11.99],["Meat Lovers",13.99], ["Sausage",13.99], ["Chicken",13.99], ["Anchovy",14.99]]
+side_list = [["Garlic Bread", 5.67], ["Fries", 4.32],["Chicken Wings", 7.59], ["Salad",4.5]]
+drinl_list = []
 def pizza_menu():
     """
-    This function presents the user with the pizza options then allows them to add 1 to their order
+    This function presents the user with the pizza options. It then allows the user to input which they would
+    like to add to their order and how many of that pizza they would like to add.
     """
-    print("Ham & Cheese $13.99\nPepperoni $12.49\nVegetarian $14.99\nCheese 11.99\nMeat Lovers $13.99\nSausage $13.99\nChicken $13.99\nAnchovy $14.99")
-    user_pizza = input("Which pizza would you like to add?\n")
-    pizza_found = False
-    for pizza in pizza_list:
-        if user_pizza == pizza[0]:
-            pizza_count = int(input("How many would you like to add?\n"))
-            for i in range(pizza_count):
-                order.append(pizza)
-            pizza_found = True
-    if pizza_found == False:
-        print("That pizza could not be found")
-        
+    print("1)Ham & Cheese $13.99\n2)Pepperoni $12.49\n3)Vegetarian $14.99\n4)Cheese 11.99\n5)Meat Lovers $13.99\n6)Sausage $13.99\n7)Chicken $13.99\n8)Anchovy $14.99")
+    user_pizza = int(input("Which pizza would you like to add?\n>"))
+    pizza_count = int(input("How many would you like to add?\n>"))
+    for i in range(pizza_count):
+        order.append(pizza_list[user_pizza-1]) 
 
 def side_menu():
-    print()
+    print("1)Garlic Bread $5.67\n2)Fries $4.32\n3)Chicken Wings $7.59\n4)Salad $4.5")
+    user_side = int(input("Which side would you like to add\n>"))
+    side_count = int(input("How many would you like to add?\n>"))
+    for i in range(side_count):
+        order.append(side_list[user_side-1])
+
+
+def input_checker():
+    print() 
+
+def welcome():
+    print("Welcome to The Great Pie in the Sky\nYou're gonna love our pizza\nMax 10 items per order")
+    input("Press enter to start your order")
 
 def drink_menu():
     print
@@ -37,6 +45,7 @@ def menu():
             drink_menu()
         elif user_choice == "d":
             check_out()
-start_order = input("Press enter to start your order")
+welcome()
+
 menu()
 pizza_menu()
